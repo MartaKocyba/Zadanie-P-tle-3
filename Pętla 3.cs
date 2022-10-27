946c65e638f3b1e433c59dcacd7b6c564f677b08
MartaKocyba <116761391+MartaKocyba@users.noreply.github.com>
2022-10-27 22:34:08 +0200

    Add files via upload

---
+++ Pętla 3.cs
@@ -0,0 +1,20 @@
+﻿int liczba = 0;
+bool sukces;
+
+
+do
+{
+    Console.Write("Podaj całkowitą liczbę dodatnią: ");
+    sukces = int.TryParse(Console.ReadLine(), out liczba);
+}
+while ((!sukces) || (liczba < 0));
+
+for (int linia = 1; linia <= liczba; linia++)
+{
+    for (int i = 1; i <= liczba; i++)
+    {
+        Console.Write("X");
+    }
+    Console.Write("\n");
+}
+Console.ReadKey();
